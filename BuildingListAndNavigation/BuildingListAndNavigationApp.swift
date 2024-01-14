580ba16d8ed97eb67c92f279305c7fea2bef306a
Maxim Kucherov <maxim.kucherov.v@gmail.com>
2024-01-14 19:17:16 +0100

    Initial Commit

---
+++ BuildingListAndNavigation/BuildingListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListAndNavigationApp.swift
+//  BuildingListAndNavigation
+//
+//  Created by Maxim Kucherov on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
